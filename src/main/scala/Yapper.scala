19b47320657086af6497ee6dab1fc9ac5a8f835d
davidruiz600 <davidruiz@ug.uchile.cl>
2025-08-09 18:20:35 -0400

    Here is my function

--- src/main/scala/Yapper.scala
+++ src/main/scala/Yapper.scala
@@ -4,8 +4,11 @@ object Yapper {
   val farewell:String=""
   def main(args: Array[String]): Unit = {
     println("Hello my fellow memeros")
+    gregory("David")
   }
-
+  def gregory(nombre: String):Unit={
+    println(s"Hello $nombre")
+  }
   def say(something:String):Unit={
 
   }
